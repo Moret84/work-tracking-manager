dfca981e80148a446896c6c3b533c935193d280b
Aurélien Rivet <aurelienrivet@hotmail.fr>
2024-02-10 21:26:21 +0100

    introduce main with dummy cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
 mod tracking_record;
 mod work_duration;
+mod tracking_day;
 
+use tracking_day::TrackingDay;
 use tracking_record::TrackingRecord;
 use work_duration::WorkDuration;
 
-use std::time::Duration;
+use clap::Parser;
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Parser)]
+struct Cli {
+    /// The output path where saving the result.
+    #[arg(short, long)]
+    output_path: Option<PathBuf>,
+
+    /// The path of the input file.
+    input_path: PathBuf,
+}
 
 fn main() {
-    let record = TrackingRecord {
-        id: "legrand 11512".to_string(),
-        description: "iOS version - Tool page is blank".to_string(),
-        time: Duration::new(70, 0)
+    let args = Cli::parse();
+
+    let tracking_days = load_tracking_days(args.input_path.to_str().unwrap());
+}
+
+fn load_tracking_days(filepath: &str) -> Vec<TrackingDay> {
+    let mut file = match File::open(filepath) {
+        Ok(file) => file,
+        Err(error) => panic!("Erreur lors de l'ouverture du fichier : {:?}", error),
     };
 
-    let serialized_yaml = serde_yaml::to_string(&record).unwrap();
+    let mut contents = String::new();
 
-    let work_duration : Result<WorkDuration, _>= "7:30".parse();
-
-    println!("{}", work_duration.as_ref().unwrap());
-    println!("{}", work_duration.unwrap().to_string());
+    match file.read_to_string(&mut contents) {
+        Ok(_) => {
+            serde_yaml::from_str(&contents).unwrap()
+        }
+        Err(error) => panic!("Erreur lors de la lecture du fichier : {:?}", error),
+    }
 }

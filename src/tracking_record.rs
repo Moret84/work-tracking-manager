cfabc8b98bf923d2b2405d8e0e1daccacc06df4b
Aurélien Rivet <aurelien@crossover.tech>
2026-06-01 09:24:05 +0200

    fix panic if description empty

--- src/tracking_record.rs
+++ src/tracking_record.rs
@@ -5,7 +5,7 @@ use crate::WorkDuration;
 #[derive(Deserialize, Serialize, Clone)]
 pub struct TrackingRecord {
     pub id: String,
-    #[serde(skip_serializing_if = "String::is_empty")]
+    #[serde(default, skip_serializing_if = "String::is_empty")]
     pub description: String,
     pub duration: WorkDuration
 }
